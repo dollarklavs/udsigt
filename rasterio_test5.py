0365c35523699f4e73eacdc9fbdcf5707cb0e985
dollarklavs <dollarklavs@gmail.com>
2018-06-03 16:08:08 +0200

    constants for input created

--- rasterio_test5.py
+++ rasterio_test5.py
@@ -12,6 +12,11 @@ shp = '/home/jonas/udsigt/barriere.shp'
 vrt = '/tmp/dsm/nine_cells3.vrt'
 out_rst = '/tmp/dsm/burn_raster.tif'
 burn_viewshed_rst = '/tmp/dsm/burn_viewshed_rst.tif'
+
+distance = 1000
+observer_height = 5
+point = (701495,6201503)
+
 with fiona.open(shp, "r") as shapefile, rasterio.open(vrt) as src_rst:
 
     dsm = src_rst.read()
@@ -43,7 +48,7 @@ with fiona.open(shp, "r") as shapefile, rasterio.open(vrt) as src_rst:
             from_np_raster[...] = new_dsm
             from_np_raster.write('ny_rast')
             print(from_np_raster)
-            gcore.run_command('r.viewshed', input='ny_rast', output='viewshed', max_distance=1000, coordinates=(701495,6201503), observer_elevation=500.0)
+            gcore.run_command('r.viewshed', input='ny_rast', output='viewshed', max_distance=distance, coordinates=point, observer_elevation=observer_height)
             #r_viewshed(input=from_np_raster, output='viewshed', max_distance=1000, memory=1424, coordinates=(701495,6201503), observer_elevation=500.0)
             r_out_gdal(overwrite=True, input='viewshed', output=burn_viewshed_rst)
 #    with rasterio.open(out_rst,'w',**out_meta) as dst: 
